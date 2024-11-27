4bb0ec64d3ed85856a05c3920043f72d1c7472fc
Crunchies3 <113993166+Crunchies3@users.noreply.github.com>
2024-11-27 20:40:55 +0800

    Added button for analyzing post

--- inline_sentiment/script.js
+++ inline_sentiment/script.js
@@ -1,10 +1,28 @@
 let text = "";
+let lastUrl = location.href;
+
+displayAnalyzeButton();
+
+new MutationObserver(() => {
+    const url = location.href;
+    if (url !== lastUrl) {
+        lastUrl = url;
+        displayAnalyzeButton();
+    }
+}).observe(document, {subtree: true, childList: true});
 
 document.addEventListener("mouseup", (event) => {
     const selectedText = window.getSelection().toString().trim();
     if (selectedText) {
         showIcon(event.pageX, event.pageY);
         text = selectedText;
+    }
+});
+
+document.addEventListener("mousedown", () => {
+    const icon = document.getElementById("sentiment-icon");
+    if (icon && event.target !== icon) {
+        icon.remove();
     }
 });
 
@@ -21,23 +39,23 @@ function showIcon(x, y) {
     document.body.appendChild(icon);
 
     icon.addEventListener("click", () => {
-        alert(text);
+        alert(text + "Analyzing Text Sentiment");
     });
 }
 
-document.addEventListener("mousedown", () => {
-    const icon = document.getElementById("sentiment-icon");
-    if (icon && event.target !== icon) {
-        icon.remove();
+function displayAnalyzeButton() {
+    const creditBar = document.querySelector('div[slot="credit-bar"]');
+
+    if (creditBar) {
+        const button = document.createElement("button");
+        button.textContent = "Analyze Post";
+        button.type = "button";
+        button.classList.add("analyze-button");
+        button.addEventListener("click", () => {
+            alert("Analyzing Post Sentiments")
+        })
+        creditBar.insertAdjacentElement('afterend', button);
     }
-});
-
-const creditBar = document.querySelector('div[slot="credit-bar"]');
-
-if (creditBar) {
-    const button = document.createElement("button");
-    button.textContent = "Analyze Post";
-    button.type = "button";
-    button.classList.add("analyze-button");
-    creditBar.insertAdjacentElement('afterend', button);
 }
+
+
